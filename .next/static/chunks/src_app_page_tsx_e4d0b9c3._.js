04d4d3a6bf42ba08a5cb40f83a76087bde577c73
Ramón Menor <ramonmenor@gmail.com>
2025-08-28 15:05:51 +0200

    Dejar de seguir

--- .next/static/chunks/src_app_page_tsx_e4d0b9c3._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_a0ee6893._.js"
-],
-    source: "dynamic"
-});
